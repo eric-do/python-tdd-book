ed41e8bd09960a5f9a4ec06bcb2ffce005a34075
Eric Do <ericdo.617@gmail.com>
2018-02-13 12:40:50 -0800

    Functional test now checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -16,17 +18,33 @@ class NewVisitorTest(unittest.TestCase):
 
         # He notices the page title and header mention todo lists
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
         # He is invited to enter a todo item straight away
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
 
         # He types "Buy a new gi" into a text box
+        inputbox.send_keys('Buy a new gi')
 
         # When he hits enter, the page updates, and the page lists
         # "1: Buy a new gi" as an item in a todo list
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy a new gi' for row in rows)
+        )
 
         # There is still a text box inviting him to add another item. 
         # He enters "Shrink gi down to fit"
+        self.fail('Finish the test!')
 
         # The page updates again, and now both items appear on the list
 
