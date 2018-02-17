d3c83778d2928980e0d9a6e3ad97ece3f043a3ea
Eric Do <ericdo.617@gmail.com>
2018-02-16 16:21:15 -0800

    Model for list Items and associated migration

--- functional_tests.py
+++ functional_tests.py
@@ -7,9 +7,15 @@ class NewVisitorTest(unittest.TestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
+        #self.browser = webdriver.Chrome(executable_path=r'C:\Program Files (x86)\Google\Chrome\Application\chromedriver.exe')
 
     def tearDown(self):
         self.browser.quit()
+
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Eric has heard about a new todo app. 
@@ -29,29 +35,32 @@ class NewVisitorTest(unittest.TestCase):
         )
 
         # He types "Buy a new gi" into a text box
-        inputbox.send_keys('Buy a new gi')
-
+        inputbox.send_keys('Buy peacock feathers')
+        
         # When he hits enter, the page updates, and the page lists
-        # "1: Buy a new gi" as an item in a todo list
+        # "1: Buy a new gi" as an item in a todo list     
+        inputbox.send_keys(Keys.ENTER) 
+        time.sleep(1)
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+
+        # There is still a text box inviting her to add another item. She
+        # enters "Use peacock feathers to make a fly" (Edith is very
+        # methodical)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy a new gi' for row in rows),
-            "New to-do item did not appear in table"
-        )
-
-        # There is still a text box inviting him to add another item. 
-        # He enters "Shrink gi down to fit"
-        self.fail('Finish the test!')
+        print(self.browser.page_source)
 
         # The page updates again, and now both items appear on the list
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
         # Eric wonders whether the site will remember his list and notices
         # the site has generated a unique URL for him - - there is some
         # explanatory text
+        print(self.browser.page_source)
 
         # He visits the URL, and his todo list is still there
 
